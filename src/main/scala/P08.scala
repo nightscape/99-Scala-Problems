b5f90d86a21b0dc87762c409fa577aee27a76f25
Martin Mauch <martin.mauch@gmail.com>
2017-06-27 16:45:18 +0200

    P08 with pattern matching

--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -2,6 +2,11 @@ package org.p99.scala
 
 object P08 {
 
-  // TODO
+  def compress[A](list: List[A]): List[A] = list match {
+    case Nil => Nil
+    case x :: Nil => x :: Nil
+    case x :: y :: rest if x == y => compress(x :: rest)
+    case x :: y :: rest => x :: compress(y :: rest)
+  }
 
 }
