bbf699727c4a4a74cbf631b2adeaff5f6ed88da0
Martin Mauch <martin.mauch@gmail.com>
2017-06-27 16:38:51 +0200

    P01 with pattern matching

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -2,6 +2,9 @@ package org.p99.scala
 
 object P01 {
 
-  // TODO
-
+  def last[T](list: List[T]): T = list match {
+    case Nil => throw new NoSuchElementException("Cannot extract last element of empty list")
+    case x :: Nil => x
+    case x :: xs => last(xs)
+  }
 }
--- src/test/scala/P01Spec.scala
+++ src/test/scala/P01Spec.scala
@@ -4,7 +4,14 @@ import org.scalatest._
 
 class P01Spec extends UnitSpec {
 
-  // TODO
+  "last" should "return the last element of the list" in {
+    assert(P01.last(List(1)) == 1)
+    assert(P01.last(List(1,2)) == 2)
+  }
 
+  "last" should "return throw an exception if the list is empty" in {
+    assertThrows[NoSuchElementException] {
+      P01.last(List())
+    }
+  }
 }
-
